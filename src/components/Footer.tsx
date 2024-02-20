3dba0022e46c07f0633d196ef93608c910dab41a
Mickael Riss <mendos.prod@gmail.com>
2024-02-19 23:01:51 -0500

    Create repo

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <div className="flex h-24 w-full items-center justify-center border-t">
+      <h4>Footer</h4>
+      <a
+        className="flex items-center justify-center gap-2"
+        href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Powered by{" "}
+      </a>
+    </div>
+  );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <div className="flex h-16 w-full items-center justify-center border-b">
+      <h4>Navbar</h4>
+      <a
+        className="flex items-center justify-center gap-2"
+        href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+      </a>
+    </div>
+  );
+}
