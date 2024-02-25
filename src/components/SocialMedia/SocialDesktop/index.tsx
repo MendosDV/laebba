8ae0ada4b3614547c76a87db51ac7c260ef64aaa
Mickael Riss <mendos.prod@gmail.com>
2024-02-24 21:02:15 -0500

    Add SocialMediaDesktop

--- src/components/SocialMedia/SocialDesktop/index.tsx
+++ src/components/SocialMedia/SocialDesktop/index.tsx
@@ -6,13 +6,26 @@ import { useSizeScreen } from "@/context";
 
 export default function SocialDesktop() {
   const { isMobile } = useSizeScreen();
+  const socialMedia = [
+    { name: 'Linkedin',
+      icon: <TiSocialLinkedin style={{fontSize: '1.4rem'}} aria-label='Linkedin'/>,
+      href: 'https://www.linkedin.com/in/mickael-riss'
+    },
+    { name: 'Github',
+      icon: <FaGithub style={{fontSize: '1.4rem'}} aria-label='Github'/>,
+      href: 'https://github.com/MendosDV'
+    }
+  ]
 
   {
     if (!isMobile) {
       return (
-        <div className='absolute top-1/2 flex flex-col gap-3 bg-black'>
-          <TiSocialLinkedin style={{fontSize: '2rem'}} aria-label='Linkedin'/>
-          <FaGithub style={{fontSize: '2rem'}} aria-label='Github'/>
+        <div className='absolute top-1/2 flex flex-col gap-5'>
+        {socialMedia.map(({ name, icon, href }) => (
+            <a href={href} key={name} target='_blank' className="rounded-full p-2 bg-white shadow-lg">
+              {icon}
+            </a>
+        ))}
       </div>
       )
     }
